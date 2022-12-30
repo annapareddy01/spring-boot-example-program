206af2dfef130c1ce549e1cbf71d88ad4f0f1954
manviswa <viswamandem@gmail.com>
2022-12-30 14:55:42 +0530

    initial programme to return JSON as a response

--- src/main/java/com/bhujatha/springbootexampleprogram/controller/HelloController.java
+++ src/main/java/com/bhujatha/springbootexampleprogram/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.bhujatha.springbootexampleprogram.controller;
 
+import com.bhujatha.springbootexampleprogram.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,5 +12,14 @@ public class HelloController {
     @RequestMapping("/")
     public String hello(){
         return "Hello World!!";
+    }
+
+    @RequestMapping("/getUser")
+    public User getUser(){
+        User user = new User();
+        user.setId(1);
+        user.setName("Bhujatha");
+        user.setEmailId("bhujatha92@gmail.com");
+        return user;
     }
 }
