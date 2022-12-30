2550a056450818286e9c1477173895784bd37ca8
manviswa <viswamandem@gmail.com>
2022-12-30 11:17:54 +0530

    Created initial Spring boot application

---
+++ src/test/java/com/bhujatha/springbootexampleprogram/SpringBootExampleProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhujatha.springbootexampleprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
